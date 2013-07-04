272d2e3843d600481f0b810eb374d21f4c27ca34
Li Yuhang <ryenlea@gmail.com>
2013-07-04 22:43:48 +0800

    init

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '5.1'
+
+pod 'AFNetworking', '~> 1.3.0'
+pod 'Underscore.m', '~> 0.2.0'
+pod 'NoticeView', '~> 3.0'
+pod 'NUI'
---
+++ listen50/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  listen50
+//
+//  Created by Li Yuhang on 13-7-4.
+//  Copyright (c) 2013年 Li Yuhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
